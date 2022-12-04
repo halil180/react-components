f16996ba1fed48eedfe65bf8e0fb9a83efbb0c2d
halil180 <halilkom180@.com>
2022-12-04 15:59:18 +0100

    functional components

---
+++ src/components/Content/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./content.css"
+function Content() {
+  return (
+    <div>
+        <article>
+        <h1>lorem</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo at odio numquam? Velit vel assumenda sapiente a officia dolore dolores.</p>
+        </article>
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./header.css"
+function Header() {
+  return (
+    <ul>
+        <li><a href="https://hicoders.ch">Home</a></li>
+        <li><a href="https://hicoders.ch">About us</a></li>
+        <li><a href="https://hicoders.ch">Contact</a></li>
+        <li><a href="https://hicoders.ch">Blog</a></li>
+    </ul>
+  )
+}
+
+export default Header
